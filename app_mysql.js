cf4422092f0779ddd8f20937cfc6d1dbabb52662
0minKoh <esesang13@naver.com>
2022-04-20 09:18:37 +0900

    데이터 추가 post

--- app_mysql.js
+++ app_mysql.js
@@ -4,6 +4,10 @@ const mysql = require("./mysql/index.js")
 
 const app = express()
 
+app.use(express.json({
+  limit: '50mb'
+}))
+
 app.listen(3000, () => {
   console.log('Server started. port 3000')
 })
@@ -13,4 +17,9 @@ app.get('/api/customers', async (req, res) => {
   const customers = await mysql.query('customerList') //customerList의 query실행
   console.log(customers)
   res.send(customers)
+})
+
+app.post('/api/customers/insert', async (req, res) => {
+  const result = await mysql.query('customerInsert', req.body.param)
+  res.send(result)
 })
